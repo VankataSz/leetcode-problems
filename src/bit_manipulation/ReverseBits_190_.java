30d77ea7f507df32e3f14c827892fdbdad716cf9
VankataSz <ian_it@abv.bg>
2024-04-10 11:21:55 +0300

    Up!

---
+++ src/bit_manipulation/ReverseBits_190_.java
@@ -0,0 +1,28 @@
+package bit_manipulation;
+
+public class ReverseBits_190_ {
+    public static void main(String[] args) {
+
+        int result = reverseBits(43261596);
+        System.out.println(result);
+
+    }
+
+
+
+    // you need treat n as an unsigned value
+    public static int reverseBits(int n) {
+
+        int ans = 0;
+
+        for (int i = 0; i < 32; i++) {
+            ans <<= 1;
+            ans |= (n & 1);
+            n >>= 1;
+        }
+        return ans;
+    }
+}
+
+
+
